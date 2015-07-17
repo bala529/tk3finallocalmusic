974492ffa9a98d8566bf4487f77aa169fd1e0f1f
balu <balu.ls@live.com>
2015-07-17 03:23:38 +0200

    changes

--- SocialAmbient/src/de/tu/darmstadt/tk/bonus/m1/group/project/localmusic/SubscribeLocal.java
+++ SocialAmbient/src/de/tu/darmstadt/tk/bonus/m1/group/project/localmusic/SubscribeLocal.java
@@ -5,7 +5,7 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-import de.tu.darmstadt.tk.bonus.m1.group.project.facebook.Constants;
+import de.tu.darmstadt.tk.bonus.m1.group.project.localmusic.*;
 
 /**
  * 
